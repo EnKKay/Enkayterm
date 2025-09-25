4bba6adf136ffac0ab7fa8f80f2022c9ef1fdfb5
Enkay <staticenkkay@gmail.com>
2025-09-25 10:57:26 -0400

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,84 @@
+var youtube = "https://www.youtube.com/Enkay/";
+var twitter = "https://www.twitter.com/Enkay/";
+var password = "nkcodes";
+var linkedin = "https://www.linkedin.com/in/Enkay/";
+var instagram = "https://www.instagram.com/Enkay/";
+var github = "https://github.com/Enkay/";
+var email = 'mailto:Enkay@enkkay.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Enkay!👋",
+  "I'm a software engineer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documented my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "engineering & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creation - strike while",
+  "the iron's hot.",
+  "Now, I make videos about software engineering and computer science, building anything my heart desires,",
+  "like this terminal website.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/Enkay' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/Enkay' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/Enkay' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/Enkay' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/Enkay' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Enkay?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">ENKAY (NK) Not A Corporation. All rights reserved.</span>',
+ " ░▒▓████████▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░  ",
+ " ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ",
+ " ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ",
+ " ░▒▓██████▓▒░ ░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓████████▓▒░░▒▓██████▓▒░   ",
+ " ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ",
+ " ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ",
+ " ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ",
+                                                                   
+                                                                   
+                                                           
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
